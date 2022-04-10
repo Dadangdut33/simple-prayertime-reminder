34b3fb9b2a779c690d5922cd545b38d3640a897f
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-04-10 14:17:50 +0700

    install needed packages

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
+import './Fonts.css';
 
 const Hello = () => {
   return (
