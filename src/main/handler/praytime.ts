22d4600ee121664d66dc29c748c49a0776dee8db
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-04-11 21:20:42 +0700

    get praytime for #2

---
+++ src/main/handler/praytime.ts
@@ -0,0 +1,71 @@
+import adhan, { Coordinates, CalculationMethod } from 'adhan';
+import Moment from 'moment-timezone';
+import { configInterface } from './files';
+
+const methodMap = {
+	MuslimWorldLeague: CalculationMethod.MuslimWorldLeague,
+	Egyptian: CalculationMethod.Egyptian,
+	Karachi: CalculationMethod.Karachi,
+	UmmAlQura: CalculationMethod.UmmAlQura,
+	Dubai: CalculationMethod.Dubai,
+	MoonsightingCommittee: CalculationMethod.MoonsightingCommittee,
+	NorthAmerica: CalculationMethod.NorthAmerica,
+	Kuwait: CalculationMethod.Kuwait,
+	Qatar: CalculationMethod.Qatar,
+	Singapore: CalculationMethod.Singapore,
+	Tehran: CalculationMethod.Tehran,
+	Turkey: CalculationMethod.Turkey,
+};
+
+const highLatitudeRuleMap = {
+	MiddleOfTheNight: adhan.HighLatitudeRule.MiddleOfTheNight,
+	SeventhOfTheNight: adhan.HighLatitudeRule.SeventhOfTheNight,
+	TwilightAngle: adhan.HighLatitudeRule.TwilightAngle,
+};
+
+export const getPrayerTimes = (settings: configInterface) => {
+	const date = new Date(), // Current date
+		params = methodMap[settings.calcOption.method](),
+		coordinates = new Coordinates(parseFloat(settings.locationOption.latitude), parseFloat(settings.locationOption.longitude));
+
+	// Set madhab
+	params.madhab = settings.calcOption.madhab === 'Shafi' ? adhan.Madhab.Shafi : adhan.Madhab.Hanafi;
+
+	// Set highLatitudeRule
+	params.highLatitudeRule = highLatitudeRuleMap[settings.calcOption.highLatitudeRule];
+
+	// adjustments
+	params.adjustments = {
+		fajr: settings.calcOption.adjustments.fajr,
+		sunrise: settings.calcOption.adjustments.sunrise,
+		dhuhr: settings.calcOption.adjustments.dhuhr,
+		asr: settings.calcOption.adjustments.asr,
+		maghrib: settings.calcOption.adjustments.maghrib,
+		isha: settings.calcOption.adjustments.isha,
+	};
+
+	// ----------------------------------------------------------------
+	const prayerGet = new adhan.PrayerTimes(coordinates, date, params);
+	let fajrTime = Moment(prayerGet.fajr).tz(settings.timezoneOption.timezone).format('h:mm A'),
+		sunriseTime = Moment(prayerGet.sunrise).tz(settings.timezoneOption.timezone).format('h:mm A'),
+		dhuhrTime = Moment(prayerGet.dhuhr).tz(settings.timezoneOption.timezone).format('h:mm A'),
+		asrTime = Moment(prayerGet.asr).tz(settings.timezoneOption.timezone).format('h:mm A'),
+		maghribTime = Moment(prayerGet.maghrib).tz(settings.timezoneOption.timezone).format('h:mm A'),
+		ishaTime = Moment(prayerGet.isha).tz(settings.timezoneOption.timezone).format('h:mm A'),
+		current = prayerGet.currentPrayer().toString(),
+		next = prayerGet.nextPrayer().toString();
+
+	if (next === 'none') next = 'fajr';
+
+	return {
+		prayerGet,
+		fajrTime,
+		sunriseTime,
+		dhuhrTime,
+		asrTime,
+		maghribTime,
+		ishaTime,
+		current,
+		next,
+	};
+};
