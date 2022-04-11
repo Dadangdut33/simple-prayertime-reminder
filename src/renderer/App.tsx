d19b6867cf9673db7830fe23093d739d32cfa20e
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-04-11 20:57:20 +0700

    fix typo and set window name

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
-import './Fonts.css';
+import './Font.css';
 
 const Hello = () => {
 	return (
